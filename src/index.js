e98dce899a9ccb5bb223c35504845867f4073b71
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-05-25 16:55:56 +0530

    connet to mongo db

---
+++ src/index.js
@@ -0,0 +1,33 @@
+// require('dotenv').config({ path: './env'})
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({ 
+    path: './env'
+});
+
+
+
+connectDB()
+
+/* 
+// this approach is for directly connecting db from index.js. Another approach is just calling the fuction in indejx.js where as the function is written in DB/index.js. Later approach is more modular and porfessional
+//ifis
+;( async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", () => {
+            console.error("Error", error);
+            throw err;
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Listening on ${process.env.PORT}`);
+        })
+    }catch(error){
+        console.error("ERROR", error);
+        throw err;
+    }
+})()
+
+*/
