94f6d9552640e6e081883814c47b2b75928c3631
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-05-26 21:00:33 +0530

    Util code discussion

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 // require('dotenv').config({ path: './env'})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ 
     path: './env'
 });
 
-
-
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at ${process.env.PORT}`);
+        
+    });
+})
+.catch((err) => {
+    console.log("MongoDB Connection Failed!!! ", err);
+})
 
 /* 
 // this approach is for directly connecting db from index.js. Another approach is just calling the fuction in indejx.js where as the function is written in DB/index.js. Later approach is more modular and porfessional
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+//using promise
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+export { asyncHandler }
+
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (fn) => () => {}
+// const asyncHandler = (fn) => async () => {}
+
+
+//using try catch
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try{
+//         await fn(req, res, next)
+//     }catch(error){
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
